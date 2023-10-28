f797cf87f28214053969caf932c978a87e70a350
田中　修平 <akashuhei@outlook.jp>
2023-10-28 09:16:49 +0900

    「Reportモデル、ReportViewモデル作成」

---
+++ src/main/java/actions/views/ReportView.java
@@ -0,0 +1,31 @@
+package actions.views;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter //全てのクラスフィールドについてgetterを自動生成する(Lombok)
+@Setter //全てのクラスフィールドについてsetterを自動生成する(Lombok)
+@NoArgsConstructor //引数なしコンストラクタを自動生成する(Lombok)
+@AllArgsConstructor //全てのクラスフィールドを引数にもつ引数ありコンストラクタを自動生成する(Lombok)
+public class ReportView {
+
+    private Integer id;
+
+    private EmployeeView employee;
+
+    private LocalDate reportDate;
+
+    private String title;
+
+    private String content;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+}
